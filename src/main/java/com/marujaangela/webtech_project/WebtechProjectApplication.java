5fca8aca47cb11d023b1320eb9242185814b4a2e
marujaangela <s0591654@htw-berlin.de>
2025-04-07 13:19:21 +0200

    spring page

---
+++ src/main/java/com/marujaangela/webtech_project/WebtechProjectApplication.java
@@ -0,0 +1,13 @@
+package com.marujaangela.webtech_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marujaangela/webtech_project/WebtechProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marujaangela.webtech_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
