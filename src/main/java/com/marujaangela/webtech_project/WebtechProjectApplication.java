b3dc39dcb19b739cc8ad6fa14bcab89afee15183
marujaangela <s0591654@htw-berlin.de>
2025-06-15 21:38:38 +0200

    Exclude entfernen

--- src/main/java/com/marujaangela/webtech_project/WebtechProjectApplication.java
+++ src/main/java/com/marujaangela/webtech_project/WebtechProjectApplication.java
@@ -7,12 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class,
-		DataSourceTransactionManagerAutoConfiguration.class,
-		JpaRepositoriesAutoConfiguration.class
-})
+@SpringBootApplication
 public class WebtechProjectApplication {
 
 	public static void main(String[] args) {
