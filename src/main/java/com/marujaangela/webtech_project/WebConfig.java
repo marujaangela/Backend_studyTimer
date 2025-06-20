811ee5ff4e967853c8a054cc59324faa79bc3f38
marujaangela <s0591654@htw-berlin.de>
2025-06-20 19:19:58 +0200

    Update - config

--- src/main/java/com/marujaangela/webtech_project/WebConfig.java
+++ src/main/java/com/marujaangela/webtech_project/WebConfig.java
@@ -5,13 +5,13 @@ import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://frontend-studytimer.onrender.com") // echte URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowCredentials(false);
+                .allowedOrigins("https://frontend-studytimer.onrender.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
+
 
