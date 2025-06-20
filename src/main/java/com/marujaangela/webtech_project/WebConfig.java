379c38b7980141996784ffa72804662ade881ee9
marujaangela <s0591654@htw-berlin.de>
2025-06-20 19:32:04 +0200

    Update - berechtigung link

--- src/main/java/com/marujaangela/webtech_project/WebConfig.java
+++ src/main/java/com/marujaangela/webtech_project/WebConfig.java
@@ -8,7 +8,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://frontend-studytimer.onrender.com")
+                .allowedOrigins("https://frontend-studytimer.onrender.com", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
