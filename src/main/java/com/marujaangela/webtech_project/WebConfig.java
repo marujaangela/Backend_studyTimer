1d1d955639c395dc73f79a307312be57609a5317
marujaangela <s0591654@htw-berlin.de>
2025-06-20 17:57:16 +0200

    Update - render URL Frontend

--- src/main/java/com/marujaangela/webtech_project/WebConfig.java
+++ src/main/java/com/marujaangela/webtech_project/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://dein-frontend.onrender.com") // 🔁 deine echte URL hier eintragen
+                .allowedOrigins("https://frontend-studytimer.onrender.com") // echte URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(false);
     }
